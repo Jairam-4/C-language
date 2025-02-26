d49a772823bb045ffaf01927302e8c40754de8d8
code with JaiRam <yedlajairam@gmail.com>
2025-02-26 13:18:07 +0530

    Update in a number the even num is become 0 .c
    lets us learn coding from basic to advance. learn coding C with Jai.

--- in a number thven num is become 0 .c
+++ in a number thven num is become 0 .c
@@ -1,19 +1,30 @@
 #include<stdio.h>
 int main()
 {
-    int a,b;
-    scanf("%d",&a);
-    while(a!=0)
-    {
-        b=a%10;
-        a=a/10;
-        if(b%2==0)
-        {
-            printf("0");
-        }
-        else {
-            printf("%d",b);
-        }
-        
-    }
+    int a,temp,r,rev=0,b;
+scanf("%d",&a);
+temp=a;
+while(temp!=0)
+{
+r=temp%10;
+temp=temp/10;
+rev=rev*10+r;
 }
+while(rev!=0)
+{
+b=rev%10;
+rev=rev/10;
+if(b%2==0)
+{
+printf("0");
+}
+else
+{
+printf("%d",b);
+}
+
+}
+    return 0;
+}
+
+    

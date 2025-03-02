149851d98b522eddf2d2de5917e16376c642993b
code with JaiRam <yedlajairam@gmail.com>
2025-03-02 12:57:26 +0530

    Create season .c

---
+++ season .c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+int a;
+scanf("%d",&a);
+switch(a)
+{
+case 1:
+       printf("Winter");
+       break;
+case 2: printf("Winter");
+    break;
+case 3:
+printf("Spring");
+break;
+case 4:
+printf("Spring");
+break;
+case 5: printf("spring");
+break;
+ case 6: printf("Summer");
+ break;
+ case 7: printf("Summer");
+ break;
+ case 8: printf("Summer");
+ break;
+ case 9: printf("Autumn");
+ break;
+ case 10: printf("Autumn");
+ break;
+ case 11: printf("Autumn");
+ break;
+ case 12: printf("Winter");
+ break;
+ default : printf("Invalid");
+ break;
+ }
+ return 0;
+ }

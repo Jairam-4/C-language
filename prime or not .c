9c12a7adb233c02d618d3e40ae88824089b2ac95
code with JaiRam <yedlajairam@gmail.com>
2025-03-12 13:22:09 +0530

    Update prime or not .c

--- prime or not .c
+++ prime or not .c
@@ -1,22 +1,22 @@
-#include<stdio.h>
-int main()
-{
-int a;
-scanf("%d",&a);
-for (int i=2;i<=a/2;i++)
-{
-if(a%i==0)
-{
-b+=1;
-breack;
-}
-}
-if(b==0)
-{
-printf("Prime num");
-}
-else {
-printf("Not a Prime num);
-}
-return 0;
+#include <stdio.h>
+int main() {
+    int a, b = 0;
+    scanf("%d", &a);
+    for (int i = 2; i <= a / 2; i++) 
+    {
+        if (a % i == 0) 
+        {
+            b += 1;
+            break;
+        }
+    }
+    if (b == 0 && a > 1) 
+    {
+        printf("Prime num");
+    } 
+    else 
+    {
+        printf("Not a Prime num");
+    }
+    return 0;
 }

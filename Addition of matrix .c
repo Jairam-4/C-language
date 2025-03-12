95a1d145cd73f43811158b5b0cc5db5d48d0941a
code with JaiRam <yedlajairam@gmail.com>
2025-03-12 14:20:13 +0530

    Update Addition of matrix .c

--- Addition of matrix .c
+++ Addition of matrix .c
@@ -1,19 +1,33 @@
-#include<stdio.h><
-int main()
-{
-  int a[3][3],b[3][3],c[3][3]={0};
-for (int i=0,i<3;i++)
-{ for(int j=0;j<3;j++)
-{ scanf("%d",&a[i][j]);}}
-for (int i=0,i<3;i++)
-{ for(int j=0;j<3;j++)
-{ scanf("%d",&b[i][j]);}}
-for (int i=0,i<3;i++)
-{ for(int j=0;j<3;j++)
-{  c[i][j] =a[i][j] +b[i][j];
-printf("%d\t",c[i][j]);
-}
-printf("\n");
-}
-return 0;
+#include <stdio.h>
+
+int main() {
+    int a[3][3], b[3][3], c[3][3] = {0};
+
+    // Input elements for matrix a
+    printf("Enter elements for matrix A (3x3):\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Input elements for matrix b
+    printf("Enter elements for matrix B (3x3):\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    // Add matrices a and b, store result in c
+    printf("Resultant matrix after addition:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            c[i][j] = a[i][j] + b[i][j];
+            printf("%d\t", c[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
